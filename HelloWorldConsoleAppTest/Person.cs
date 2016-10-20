2ceb12a1c0d9cf0c9a88f4cb14b9b30ad62eb66e
Erik Lövbom <erik.lovbom@studentconsulting.com>
2016-10-20 16:07:46 +0200

    Ny klass tillagd, Person

---
+++ HelloWorldConsoleAppTest/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsoleAppTest
+{
+    public class Person
+    {
+        private string DateOfBirth;
+
+        public string getDateOfBirth()
+        {
+
+            return DateOfBirth;
+        }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+    }
+}
